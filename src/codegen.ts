b13f449e176ec8c071a23f97bac0fc9ea6787ecf
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-05 00:19:47 +0700

    feat: setup graphql

---
+++ src/codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+    overwrite: true,
+    schema: "https://graphql.anilist.co",
+    documents: "src/**/*.graphql",
+    generates: {
+        "src/generated/types.ts": {
+            plugins: [
+                "typescript",
+                "typescript-operations",
+                "typescript-graphql-request",
+            ],
+        },
+        "./graphql.schema.json": {
+            plugins: ["introspection"],
+        },
+    },
+};
+
+export default config;
