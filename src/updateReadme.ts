b3690dcdb4c0bebd1ad221a0410d9f932775f944
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-06 05:26:34 +0900

    fix: reload

--- src/updateReadme.ts
+++ src/updateReadme.ts
@@ -1,10 +1,13 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import { loadAnilistData } from "./loadAnilistData";
 import { modifySVG } from "./modifySVG";
 
 const entries = await loadAnilistData("bookpanda", "COMPLETED");
 console.log(entries);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, "template/calendar.svg");
 const outputFilePath = path.join(__dirname, "generated/calendar.svg");
 const modifications = {
