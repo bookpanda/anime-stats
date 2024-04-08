43ea4f17886e9da5ebff95077bb392c4715085b7
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-09 00:37:38 +0900

    feat: basic la

--- src/lastAnimes/generateLastAnimes.ts
+++ src/lastAnimes/generateLastAnimes.ts
@@ -1,9 +1,19 @@
 export const generateLastAnimes = (entries: Entry[]) => {
-    //get 10 last animes in arr
-    const lastAnimes = entries.slice(-10);
-    for (const anime of lastAnimes) {
-        console.log(anime.media.title.english);
+    const lastEntries = entries.slice(-10).reverse();
+    let lastAnimes = "";
+    for (const entries of lastEntries) {
+        lastAnimes += `
+        <div class="la-entry">
+            <div class="la-row">
+                <h4>${entries.media.title.english}</h4>
+                <h3>${entries.score}</h3>
+            </div>
+        </div>\n`;
+        console.log(
+            entries.media.title.english,
+            entries.media.coverImage.large
+        );
     }
 
-    return "";
+    return lastAnimes;
 };
