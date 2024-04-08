d7028cf35210db79f20fddeb9d5e048d62d55516
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-09 01:02:09 +0900

    feat: 2 cols

--- src/lastAnimes/generateLastAnimes.ts
+++ src/lastAnimes/generateLastAnimes.ts
@@ -1,20 +1,26 @@
 export const generateLastAnimes = (entries: Entry[]) => {
     const lastEntries = entries.slice(-10).reverse();
-    let lastAnimes = "";
+    let lastAnimes = `<div class="la-container">`;
+    let idx = 0;
     for (const entries of lastEntries) {
+        if (idx === 5) {
+            lastAnimes += `</div>\n<div class="la-container">`;
+        }
         lastAnimes += `
         <div class="la-entry">
             <div class="la-row">
-                <h4>${entries.media.title.english}</h4>
+                <h4 class="anime-title">${entries.media.title.english}</h4>
                 <h3>${entries.score}</h3>
             </div>
-            <p>${entries.startedAt.date.toDateString()} - ${entries.completedAt.date.toDateString()}</p>
+            <p>${slashDateFormat(entries.startedAt.date)} - ${slashDateFormat(entries.completedAt.date)}</p>
         </div>\n`;
-        console.log(
-            entries.media.title.english,
-            entries.media.coverImage.large
-        );
+        idx++;
     }
+    lastAnimes += `</div>`;
 
     return lastAnimes;
+};
+
+const slashDateFormat = (date: Date) => {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 };
