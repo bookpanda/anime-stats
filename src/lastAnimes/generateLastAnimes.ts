4482145f06628e551ea3237a9b913231f6d371fa
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-09 22:17:46 +0900

    feat: days ago

--- src/lastAnimes/generateLastAnimes.ts
+++ src/lastAnimes/generateLastAnimes.ts
@@ -2,17 +2,16 @@ export const generateLastAnimes = (entries: Entry[]) => {
     const lastEntries = entries.slice(-10).reverse();
     let lastAnimes = `<div class="la-container">`;
     let idx = 0;
-    for (const entries of lastEntries) {
+    for (const entry of lastEntries) {
         if (idx === 5) {
             lastAnimes += `</div>\n<div class="la-container">`;
         }
-        lastAnimes += `
-        <div class="la-entry">
+        lastAnimes += `<div class="la-entry">
             <div class="la-row">
-                <h4 class="anime-title">${entries.media.title.english}</h4>
-                <h3>${entries.score}</h3>
+                <a href="${entry.media.siteUrl}"><h4 class="anime-title">${entry.media.title.english}</h4></a>
+                <h3>${entry.score}</h3>
             </div>
-            <p>${slashDateFormat(entries.startedAt.date)} - ${slashDateFormat(entries.completedAt.date)}</p>
+            <p>${slashDateFormat(entry.startedAt.date)} - ${slashDateFormat(entry.completedAt.date)} (${daysAgo(entry.completedAt.date)} days ago)</p>
         </div>\n`;
         idx++;
     }
@@ -23,4 +22,10 @@ export const generateLastAnimes = (entries: Entry[]) => {
 
 const slashDateFormat = (date: Date) => {
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+};
+
+const daysAgo = (date: Date) => {
+    const now = new Date();
+    const diff = now.getTime() - date.getTime();
+    return Math.floor(diff / (1000 * 60 * 60 * 24));
 };
