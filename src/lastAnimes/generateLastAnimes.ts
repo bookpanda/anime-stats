539deee29485d500e6b47f48a7fb5aa8f6c8e10b
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-11 00:33:17 +0900

    test: swap w, h

--- src/lastAnimes/generateLastAnimes.ts
+++ src/lastAnimes/generateLastAnimes.ts
@@ -34,7 +34,7 @@ const daysAgo = (date: Date) => {
 };
 
 const genImage = (entry: Entry) => {
-    return `<img src="${entry.media.coverImage.large}" alt="${entry.media.title.english}" style="height:100px;width:auto" />`;
+    return `<img src="${entry.media.coverImage.large}" alt="${entry.media.title.english}" style="width:70px;height:auto" />`;
 };
 
 const genInfo = (entry: Entry) => {
